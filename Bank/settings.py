07ca3815968dda22f4896eee5ba2f568f89c24af
KadogoKenya <catherinekanini001@gmail.com>
2023-02-22 06:37:29 +0300

    first commit

--- Bank/settings.py
+++ Bank/settings.py
@@ -97,6 +97,9 @@ DATABASES = {
 }
 
 
+DEBUG = False
+ALLOWED_HOSTS = ['127.0.0.1', 'www.yourdomain.com']
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -144,10 +147,18 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+# if DEBUG:
+#     STATICFILES_DIRS = [
+#         os.path.join(BASE_DIR, 'static')
+#     ]
+# else:
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
 # LOGIN_REDIRECT_URL='afterlogin_view'
